4a86fdf67f1307ad7e1e6913d3a327a6cd3aae46
ahmedsakr <ahmed@sakr.ca>
2019-08-09 13:16:05 -0400

    Use scss and rfs

--- stocks/src/App.tsx
+++ stocks/src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
+import {Row, Col, Container} from 'react-bootstrap'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Container fluid={true}>
+        <Row id="main-block">
+          <Header />
+        </Row>
+      </Container>
     </div>
   );
+}
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <Col
+        id="navigation"
+        className="text-center p-0"
+        xs={{ span:12 }}
+        sm={{ span: 10, offset: 1 }} >
+
+        <p id="navigation-header-primary">Trump Financial</p>
+        <p id="navigation-header-secondary">Richer Yesterday</p>
+      </Col>
+    </>
+  )
 }
 
 export default App;
