3792a40024e7faec5212d535a9d68e7d4e4a0d28
joshcampiteli <joshcampitelli@cmail.carleton.ca>
2019-08-09 16:54:10 -0400

    Added npm module to retreive stock data

--- stocks/src/server.ts
+++ stocks/src/server.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import httpServer from 'http';
 import SocketIO from 'socket.io';
+import { lookup, history } from 'yahoo-stocks';
+import symbols from './snp500'
 
 let app: Express = express();
 let http: httpServer.Server = new httpServer.Server(app);
@@ -15,16 +17,33 @@ app.get('/server', (_req, res) => {
     res.send({ express: 'The Express Server is Connected to React' });
 });
 
+let number = getRandomInt();
+lookup(symbols[number]).then((response:string) => {
+    console.log(response)
+});
+
 io.on('connection', (socket: SocketIO.Socket) => {
     console.log('Client Has Connected, id: ' + socket.id)
     socket.on('tweet', (data: string) => {
         socket.emit('transformedTweet', applyTransformation(data))
+    });
+
+    socket.on('stock data', (symbol: string) => {
+        lookup(symbol).then((response:string) => {
+            socket.emit('stock data', response);
+        });
     });
 });
 
 function applyTransformation(tweet: string):string {
     return 'sell AMD';
 }
+
+function getRandomInt() {
+    let min = Math.ceil(0);
+    let max = Math.floor(498);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
 http.listen(port, () => {
     console.log('listening on *:' + port);
